00fd4632844ab440ee490f5a7c9064accb16b362
m-goryunov <108279048+m-goryunov@users.noreply.github.com>
2023-02-15 13:58:50 +0300

    ТЗ 7 Initial

---
+++ test/EpicTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EpicTest {
+
+    @Test
+    void addRelatedSubtaskIds() {
+        Assertions.assertEquals(2,2);
+    }
+}
